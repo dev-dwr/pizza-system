6017f5ba978d1bd4e34bb83f92f6a3149e243ab0
dev-dwr <dawid.jscript@gmail.com>
2021-10-09 20:06:20 +0200

    tested functionalities and configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+const testFetch = () => {
+    axios.get("http://localhost:8080/api/v1").then(res =>{
+      console.log(res);
+    })
+}
 
 function App() {
+  useEffect(() => {
+    testFetch();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
